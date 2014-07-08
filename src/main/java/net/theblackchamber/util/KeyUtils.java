40a361db857a7ae556ba64af5b4ba2c31efa480e
Seamus <sminogue@yahoo.com>
2014-07-07 23:07:07 -0400

    Added read key from disk support

--- src/main/java/net/theblackchamber/util/KeyUtils.java
+++ src/main/java/net/theblackchamber/util/KeyUtils.java
@@ -1,10 +1,15 @@
 package net.theblackchamber.util;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -81,6 +86,42 @@ public class KeyUtils {
 				try {
 					output.flush();
 					output.close();
+				} catch (Exception e) {
+					log.warn(
+							"Failed to properly close keyfile: "
+									+ e.getMessage(), e);
+				}
+			}
+
+		}
+	}
+
+	/**
+	 * Utility method which will read a {@link SymetricSerializableKey} key from
+	 * disk.
+	 * 
+	 * @param keyFile
+	 * @param key
+	 */
+	public static SymetricSerializableKey readSymetricSerializableKey(
+			File keyFile) {
+		ObjectInput input = null;
+		try {
+			InputStream file = new FileInputStream(keyFile);
+			InputStream buffer = new BufferedInputStream(file);
+			input = new ObjectInputStream(buffer);
+
+			return (SymetricSerializableKey) input.readObject();
+
+		} catch (Exception e) {
+			log.error(
+					"Failed to write symetric key to disk: " + e.getMessage(),
+					e);
+			return null;
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
 				} catch (Exception e) {
 					log.warn(
 							"Failed to properly close keyfile: "
--- src/test/java/net/theblackchamber/util/KeyUtilsTest.java
+++ src/test/java/net/theblackchamber/util/KeyUtilsTest.java
@@ -7,6 +7,7 @@ import java.io.File;
 import net.theblackchamber.key.SymetricSerializableKey;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -15,11 +16,12 @@ public class KeyUtilsTest {
 
 	@Rule
 	public TemporaryFolder tempFolder = new TemporaryFolder();
-	
+
 	@Test
 	public void testGenerateSymetricSerializableKey() {
 		try {
-			SymetricSerializableKey key = KeyUtils.generateSymetricSerializableKey();
+			SymetricSerializableKey key = KeyUtils
+					.generateSymetricSerializableKey();
 
 			assertNotNull(key);
 			assertNotNull(key.getSecretKey());
@@ -31,27 +33,57 @@ public class KeyUtilsTest {
 	}
 
 	@Test
-	public void testWriteSymetricSerializableKey(){
-		
+	public void testWriteSymetricSerializableKey() {
+
 		try {
-			SymetricSerializableKey key = KeyUtils.generateSymetricSerializableKey();
+			SymetricSerializableKey key = KeyUtils
+					.generateSymetricSerializableKey();
 
 			assertNotNull(key);
 			assertNotNull(key.getSecretKey());
 
 			File file = tempFolder.newFile("private.key");
 			assertTrue(file.exists());
-			
+
 			KeyUtils.writeSymetricSerializableKey(file, key);
-			
+
 			assertTrue(file.exists());
 			assertTrue(FileUtils.sizeOf(file) > 0);
-			
+
 		} catch (Throwable t) {
 			t.printStackTrace();
 			fail();
 		}
-		
+
 	}
-	
+
+	@Test
+	public void testReadSymetricSerializableKey() {
+
+		try {
+			SymetricSerializableKey key = KeyUtils
+					.generateSymetricSerializableKey();
+
+			assertNotNull(key);
+			assertNotNull(key.getSecretKey());
+
+			File file = tempFolder.newFile("private.key");
+			assertTrue(file.exists());
+
+			KeyUtils.writeSymetricSerializableKey(file, key);
+
+			assertTrue(file.exists());
+			assertTrue(FileUtils.sizeOf(file) > 0);
+
+			SymetricSerializableKey readKey = KeyUtils
+					.readSymetricSerializableKey(file);
+			assertTrue(StringUtils.equals(readKey.getSecretKey(),
+					key.getSecretKey()));
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+			fail();
+		}
+
+	}
 }
