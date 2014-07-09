f99a4fa266afe4c4bf641c8622e1d4b8ef8ed10a
Seamus <sminogue@yahoo.com>
2014-07-08 23:22:37 -0400

    Began work making SecurePropertiesProvider override Properties methods so that instances of it can be used seemlessly in place of Properties

--- src/main/java/net/theblackchamber/crypto/providers/SecurePropertiesProvider.java
+++ src/main/java/net/theblackchamber/crypto/providers/SecurePropertiesProvider.java
@@ -3,12 +3,55 @@
  */
 package net.theblackchamber.crypto.providers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 /**
  * @author sminogue
- *
+ * 
  */
 public class SecurePropertiesProvider extends Properties {
+
+	private static final long serialVersionUID = 6795084558089471182L;
+
+	/**
+	 * Default constructor. Uses KEYSTORE_PATH system property to locate
+	 * keystore on disk.
+	 */
+	public SecurePropertiesProvider() {
+		super();
+		// Load crypto
+	}
+
+	/**
+	 * Constructor which specifies {@link Properties} defaults. Uses
+	 * KEYSTORE_PATH system property to locate keystore on disk.
+	 */
+	public SecurePropertiesProvider(Properties defaults) {
+		super(defaults);
+		// Load crypto
+	}
+
+	@Override
+	public synchronized void load(InputStream inStream) throws IOException {
+		super.load(inStream);
+		// Load crypto
+	}
+
+	@Override
+	public synchronized void load(Reader reader) throws IOException {
+		super.load(reader);
+		// Load crypto
+	}
+
+	@Override
+	public synchronized void loadFromXML(InputStream in) throws IOException,
+			InvalidPropertiesFormatException {
+		this.loadFromXML(in);
+		// Load crypto
+	}
 
 }
