a47d7f581171041c44f162f71ddaa3b3c163492b
Seamus <sminogue@yahoo.com>
2015-06-14 13:44:49 -0400

    Improved unit test to ensure hashing was resulting in different hashes for inputs and that over a small sample size we arent seeing collisions.

--- src/test/java/net/theblackchamber/crypto/providers/SHA256DigestProviderTest.java
+++ src/test/java/net/theblackchamber/crypto/providers/SHA256DigestProviderTest.java
@@ -1,8 +1,12 @@
 package net.theblackchamber.crypto.providers;
 
-import net.theblackchamber.crypto.providers.digest.SHA256DigestProvider;
+import java.util.HashSet;
+import java.util.Set;
 
+import net.theblackchamber.crypto.providers.digest.SHA256DigestProvider;
 import static org.junit.Assert.*;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
 public class SHA256DigestProviderTest {
@@ -27,6 +31,24 @@ public class SHA256DigestProviderTest {
 				assertTrue(prevHash.equals(hashed));
 				prevHash = hashed;
 			}
+			
+		}
+		
+		Set<String> hashes = new HashSet<String>();
+		Set<String> clears = new HashSet<String>();
+		for(int i = 0 ; i <1000;i++){
+			
+			String clearString = RandomStringUtils.randomAlphanumeric(50);
+			
+			while(clears.contains(clearString)){
+				clearString = RandomStringUtils.randomAlphanumeric(50);
+			}
+			
+			String hashed = provider.digest(clearString);
+			
+			assertFalse(hashes.contains(hashed));
+			
+			hashes.add(hashed);
 			
 		}
 		
--- src/test/java/net/theblackchamber/crypto/providers/WhirlpoolDigestProviderTest.java
+++ src/test/java/net/theblackchamber/crypto/providers/WhirlpoolDigestProviderTest.java
@@ -1,9 +1,13 @@
 package net.theblackchamber.crypto.providers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import net.theblackchamber.crypto.providers.digest.SHA256DigestProvider;
 import net.theblackchamber.crypto.providers.digest.WhirlpoolDigestProvider;
 import static org.junit.Assert.*;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
 public class WhirlpoolDigestProviderTest {
@@ -28,6 +32,24 @@ public class WhirlpoolDigestProviderTest {
 				assertTrue(prevHash.equals(hashed));
 				prevHash = hashed;
 			}
+			
+		}
+		
+		Set<String> hashes = new HashSet<String>();
+		Set<String> clears = new HashSet<String>();
+		for(int i = 0 ; i <1000;i++){
+			
+			String clearString = RandomStringUtils.randomAlphanumeric(50);
+			
+			while(clears.contains(clearString)){
+				clearString = RandomStringUtils.randomAlphanumeric(50);
+			}
+			
+			String hashed = provider.digest(clearString);
+			
+			assertFalse(hashes.contains(hashed));
+			
+			hashes.add(hashed);
 			
 		}
 		
